b08666167e2189e9d023d1166cf2498bc959680b
999monkeyfu <peter.lin@concordusa.com>
2021-08-03 11:52:52 -0400

    adding monorail skyway icon

--- website/src/App.js
+++ website/src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="App-grid">
         <div className="App-grid-item">heavenly pot stickers</div><div className="App-grid-item">Ding Tai Fung</div><div className="Grid-item-bus">Greyhound Bus</div><div className="App-grid-item">Jasmine Palace</div><div className="App-grid-item">No more ingredients</div>
         <div className="App-grid-item">Iced Thai Dream</div><div ></div><div ></div><div ></div><div className="App-grid-item">Little Love</div>
-        <div className="Grid-item-train">central station</div><div ></div><div >DimSum</div><div ></div><div className="Grid-item-train">skyway Station</div>
+        <div className="Grid-item-train">central station</div><div ></div><div >DimSum</div><div ></div><div className="Grid-item-skyway">skyway Station</div>
         <div className="App-grid-item">bobba city</div><div ></div><div ></div><div ></div><div className="App-grid-item">Pho Now #5</div>
         <div className="App-grid-item">start</div><div className="App-grid-item">Dragon Dumplings</div><div className="App-grid-item">Peddle Power Hut</div><div className="App-grid-item">Scallion House</div><div className="App-grid-item">Oolong Temple</div>
       </div>

7998bdcd9cf995c9b77bc6694d8f73341d14ef3c
999monkeyfu <peter.lin@concordusa.com>
2021-08-04 08:35:24 -0400

    putting copy of graphics in src folder so it is relative

--- website/src/App.js
+++ website/src/App.js
@@ -32,7 +32,7 @@ function App() {
     <div className="App">
       <div>Logo</div>
       <div>Next Player: </div>
-      <div>last roll</div>
+      <div><img src="./resources/dice_red.gif"></img></div>
       <div className="Left-menu">
             <button onClick={handleAddPlayer}>Add Player</button>
             <button onClick={handleStartGame}>Start Game</button>
@@ -44,7 +44,7 @@ function App() {
       <div className="App-grid">
         <div className="App-grid-item">heavenly pot stickers</div><div className="App-grid-item2">Ding Tai Fung</div><div className="Grid-item-bus">Greyhound Bus</div><div className="App-grid-item2">Jasmine Palace</div><div className="App-grid-item"><p>No more ingredients</p><p>loose 1 turn</p></div>
         <div className="App-grid-item2">Iced Thai Dream</div><div ></div><div ></div><div ></div><div className="App-grid-item2">Little Love</div>
-        <div className="Grid-item-train">central station</div><div ></div><div >DimSum<img src="http://localhost:3000/resources/guabao.svg"></img></div><div ></div><div className="Grid-item-skyway">skyway Station</div>
+        <div className="Grid-item-train">central station</div><div ></div><div >DimSum<img src="./resources/guabao.svg"></img></div><div ></div><div className="Grid-item-skyway">skyway Station</div>
         <div className="App-grid-item2">bobba city</div><div ></div><div ></div><div ></div><div className="App-grid-item2">Pho Now #5</div>
         <div className="App-grid-item">start</div><div className="App-grid-item2">Dragon Dumplings</div><div className="Grid-item-car">Shared Ride</div><div className="App-grid-item2">Scallion House</div><div className="App-grid-item">Oolong Temple</div>
       </div>

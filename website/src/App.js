151246a6696498c95ddeefecc34b9575d132808a
999monkeyfu <peter.lin@concordusa.com>
2021-08-04 08:50:51 -0400

    adding id to each div tag, so we can easily look it up in javascript

--- website/src/App.js
+++ website/src/App.js
@@ -6,23 +6,23 @@ import { useState, Component, useRef } from 'react';
 const LOCAL_STORAGE_KEY = 'dimsum.data'
 
 function handleAddPlayer(e) {
-
+  console.log("add a new player");
 }
 
 function handleStartGame(e) {
-
+  console.log("start the game");
 }
 
 function handleRollDice(e) {
-
+  console.log("roll the dice if the game has started");
 }
 
 function handleChance(e) {
-
+  console.log("get a new chance card");
 }
 
 function handleBlessing(e) {
-
+  console.log("get a new blessing from a chef");
 }
 
 function App() {
@@ -42,11 +42,11 @@ function App() {
         </div>
       {/* div for the game grid */}
       <div className="App-grid">
-        <div className="App-grid-item">heavenly pot stickers</div><div className="App-grid-item2">Ding Tai Fung</div><div className="Grid-item-bus">Greyhound Bus</div><div className="App-grid-item2">Jasmine Palace</div><div className="App-grid-item"><p>No more ingredients</p><p>loose 1 turn</p></div>
-        <div className="App-grid-item2">Iced Thai Dream</div><div ></div><div ></div><div ></div><div className="App-grid-item2">Little Love</div>
-        <div className="Grid-item-train">central station</div><div ></div><div >DimSum<img src="./resources/guabao.svg"></img></div><div ></div><div className="Grid-item-skyway">skyway Station</div>
-        <div className="App-grid-item2">bobba city</div><div ></div><div ></div><div ></div><div className="App-grid-item2">Pho Now #5</div>
-        <div className="App-grid-item">start</div><div className="App-grid-item2">Dragon Dumplings</div><div className="Grid-item-car">Shared Ride</div><div className="App-grid-item2">Scallion House</div><div className="App-grid-item">Oolong Temple</div>
+        <div className="App-grid-item" id="sq5">heavenly pot stickers</div><div className="App-grid-item2" id="sq6">Ding Tai Fung</div><div className="Grid-item-bus" id="sq7">Greyhound Bus</div><div className="App-grid-item2" id="sq8">Jasmine Palace</div><div className="App-grid-item" id="sq9"><p>No more ingredients</p><p>loose 1 turn</p></div>
+        <div className="App-grid-item2" id="sq4">Iced Thai Dream</div><div ></div><div ></div><div ></div><div className="App-grid-item2" id="sq10">Little Love</div>
+        <div className="Grid-item-train" id="sq3">central station</div><div ></div><div >DimSum<img src="./resources/guabao.svg"></img></div><div ></div><div className="Grid-item-skyway" id="sq11">skyway Station</div>
+        <div className="App-grid-item2" id="sq2">bobba city</div><div ></div><div ></div><div ></div><div className="App-grid-item2" id="sq12">Pho Now #5</div>
+        <div className="App-grid-item" id="sq1">start</div><div className="App-grid-item2" id="sq17">Dragon Dumplings</div><div className="Grid-item-car" id="sq16">Shared Ride</div><div className="App-grid-item2" id="sq14">Scallion House</div><div className="App-grid-item" id="sq13">Oolong Temple</div>
       </div>
       <Players playerlist={playerarray}/>
     </div>
